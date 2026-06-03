7c5ac73d06a181f0da4aa5458872247023ba1241
Meet Jethwa <jethwa.mb@somaiya.edu>
2026-06-04 00:56:07 +0530

    Done

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: true,
-    proxy: {
-      '/api': {
-        target: 'https://blockchain-document-verification-system-hdpm.onrender.com',
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
 })
